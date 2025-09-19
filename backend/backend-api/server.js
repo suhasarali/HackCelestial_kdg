4048b86cc035e83b20423501796ada7e9df6bb3e
Rohan Bhangale <154115899+rohanb1022@users.noreply.github.com>
2025-09-19 12:17:31 +0530

    basic setup for the backend , mongo db connected

---
+++ backend/backend-api/server.js
@@ -0,0 +1,41 @@
+// server.js
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+
+
+// import weatherRoutes from "./routes/weatherRoutes.js";
+// import fishRoutes from "./routes/fishRoutes.js";
+// import mapRoutes from "./routes/mapRoutes.js";
+// import alertRoutes from "./routes/alertRoutes.js";
+// import mentorshipRoutes from "./routes/mentorshipRoutes.js";
+// import communityRoutes from "./routes/communityRoutes.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+// app.use("/api/weather", weatherRoutes);
+// app.use("/api/fish", fishRoutes);
+// app.use("/api/maps", mapRoutes);
+// app.use("/api/alerts", alertRoutes);
+// app.use("/api/mentorship", mentorshipRoutes);
+// app.use("/api/community", communityRoutes);
+
+// Root Route
+app.get("/", (req, res) => {
+  res.send("Hackathon Project API is running 🚀");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+  connectDB();
+});
